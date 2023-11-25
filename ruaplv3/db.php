afc5f07ae399c01c1fbbadeb03e20574bbea467b
milicevicmelani <73944625+milicevicmelani@users.noreply.github.com>
2023-11-25 12:20:28 +0100

    Add files via upload

---
+++ ruaplv3/db.php
@@ -0,0 +1,31 @@
+<?php
+ $host = "mmilicevic-server.mysql.database.azure.com";
+ $user = "mksmgydeva";
+ $pwd = "S821811370185A1M$";
+ $db = "mmilicevic-database";
+ // $host = "127.0.0.1:52932";
+ // $user = "azure";
+ // $pwd = "6#vWHD_$";
+ // $db = "localdb";
+ $conn = mysqli_connect($host,$user,$pwd);
+ // Check connection
+ if (mysqli_connect_errno())
+ {
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+ else {
+ echo 'Connected successfully';
+ }
+
+ mysqli_select_db($conn,$db);
+
+ $sql_create = "CREATE TABLE registration_tbl(id INT NOT NULL
+ AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(30), email
+ VARCHAR(30), date DATE);";
+ if ($conn->query($sql_create) === TRUE) {
+ echo "Table registration_tbl created successfully";
+ } else {
+ echo "Error creating table: " . $conn->error;
+ }
+ $conn->close();
+?>
